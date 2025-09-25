9694d3e788d079884aaf9f4da95cce2a3d2bd165
mLiesse <marine.liesse@gmail.com>
2025-09-25 16:15:14 +0200

    Make testing cases

--- test_pythogorean_triplets_z_limited.py
+++ test_pythogorean_triplets_z_limited.py
@@ -1,5 +1,11 @@
+import pytest
+
 from test import solve_pythagorean_triplets_z_limited
 
-def test_pytogorean_triplets_z_limited():
+def test_pytogorean_triplets_z_limited_zero_entry():
     
     assert solve_pythagorean_triplets_z_limited(0) == []
+
+def test_pytogorean_triplets_z_limited_invalid_entry_type():
+    with pytest.raises(TypeError):
+        solve_pythagorean_triplets_z_limited("a")
